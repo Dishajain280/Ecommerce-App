e99004dc14d6eb1f19aec35723390838b019d44c
Dishajain280 <dishapirodiya@gmail.com>
2026-08-11 18:50:52 +0530

    Post deployment error resolution

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/Ecommerce-app/" : "/Ecommerce-app/",
+  base: command === "build" ? "/Ecommerce-App/" : "/",
 }));
